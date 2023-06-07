58088078e00adfecc305337535903dad9928462e
fgorsky <fedor@support-pc.org>
2023-06-07 07:57:56 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fgh151/goutils
+
+go 1.20
---
+++ security.go
@@ -0,0 +1,22 @@
+package goutils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"math/rand"
+)
+
+func GetPasswordHash(password string) string {
+	hashes := md5.New()
+	hashes.Write([]byte(password))
+	return hex.EncodeToString(hashes.Sum(nil))
+}
+
+func RandString(n int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
+	}
+	return string(b)
+}
