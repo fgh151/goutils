b166b862c8a81b3cfd14b146f12e1e104c802325
fgorsky <fedor@support-pc.org>
2024-07-19 09:22:54 +0300

    data types

--- db/types.go
+++ db/types.go
@@ -66,3 +66,53 @@ func (v *NullString) Value() (driver.Value, error) {
 	}
 	return v.String, nil
 }
+
+type JsonNullInt64 sql.NullInt64
+
+func (v *JsonNullInt64) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.Int64)
+	} else {
+		return json.Marshal(nil)
+	}
+}
+
+func (v *JsonNullInt64) UnmarshalJSON(data []byte) error {
+	// Unmarshalling into a pointer will let us detect null
+	var x *int64
+	if err := json.Unmarshal(data, &x); err != nil {
+		return err
+	}
+	if x != nil {
+		v.Valid = true
+		v.Int64 = *x
+	} else {
+		v.Valid = false
+	}
+	return nil
+}
+
+type JsonNullInt32 sql.NullInt32
+
+func (v *JsonNullInt32) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.Int32)
+	} else {
+		return json.Marshal(nil)
+	}
+}
+
+func (v *JsonNullInt32) UnmarshalJSON(data []byte) error {
+	// Unmarshalling into a pointer will let us detect null
+	var x *int32
+	if err := json.Unmarshal(data, &x); err != nil {
+		return err
+	}
+	if x != nil {
+		v.Valid = true
+		v.Int32 = *x
+	} else {
+		v.Valid = false
+	}
+	return nil
+}
